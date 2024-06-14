b51d898ede400ffa4dedf5f977fbe944a285225c
Josiah Lund <josiahlund@gmail.com>
2024-06-13 21:39:36 -0700

    Add detail view, css format

--- mysite/blogging/views.py
+++ mysite/blogging/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
-from django.template import loader
 from .models import Post
 
 
@@ -20,3 +19,13 @@ def list_view(request):
     posts = published.order_by('-published_date')
     context = {'posts': posts}
     return render(request, 'blogging/list.html', context)
+
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+    context = {'post': post}
+    return render(request, 'blogging/detail.html', context)
