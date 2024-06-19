a3cc63bbfa834708d647aab1145ec9aaa1ec8ca6
Josiah Lund <josiahlund@gmail.com>
2024-06-18 22:13:22 -0700

    Remove stub_view

--- mysite/blogging/views.py
+++ mysite/blogging/views.py
@@ -3,17 +3,6 @@ from django.shortcuts import render
 from .models import Post
 
 
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
-
-
 def list_view(request):
     published = Post.objects.exclude(published_date__exact=None)
     posts = published.order_by('-published_date')
